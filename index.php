ada64ff37a2c31b2a844d3fadfa75c2bf4c13285
eka yuniar <sayayuniar@gmail.com>
2022-11-15 20:06:16 +0700

    Perintah Tugas 4

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
 $nama = "Eka Yuniar";
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar php</title>
-</head>
-
-<body>
-    <!-- Tugas 3 -->
-    <h1>Selamat Datang
-        <?php echo $nama; ?></h1>
-
-</body>
-
-</html>
+echo "Hallo, Selamat Datang $nama </br>";
+echo 'Hallo, Selamat Datang $nama';
